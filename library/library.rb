3bede84a046829462d115f608f673847ad043180
Katie Jeffree <k.jeffree@gmail.com>
2018-09-17 18:09:20 +0100

    completed all tasks

--- library/library.rb
+++ library/library.rb
@@ -11,11 +11,37 @@ attr_accessor :books
     for book in @books
       if book[:title] == requested_book
         return book
-      end 
+      end
     end
+    return nil
   end
 
+  def return_rental_details(requested_book)
+    for book in @books
+      if book[:title] == requested_book
+        return book[:rental_details]
+      end
+    end
+    return nil
+  end
 
+  def checkout_book(book_to_checkout)
+    @books << {
+      title: book_to_checkout,
+      rental_details: {
+        student_name: "",
+        date: ""
+      }
+    }
+  end
 
+  def add_rental_details(book_name, student_name, return_date)
+    for book in @books
+      if book[:title] == book_name
+        book[:rental_details][:student_name] = student_name
+        book[:rental_details][:date] = return_date
+      end
+    end
+  end
 
 end
--- library/specs/library_tests.rb
+++ library/specs/library_tests.rb
@@ -32,16 +32,46 @@ class TestLibrary < MiniTest::Test
 
   end
 
-def test_return_books
-  library = Library.new(@books)
-  assert_equal(@books, library.books)
-end
+  def test_return_books
+    library = Library.new(@books)
+    assert_equal(@books, library.books)
+  end
 
-def test_return_all_info
-  library = Library.new(@books)
-  found_book = library.return_all_info("harry_potter")
-  assert_equal(found_book, @books[1])
-end
+  def test_return_all_info__book_found
+    library = Library.new(@books)
+    found_book = library.return_all_info("harry_potter")
+    assert_equal(found_book, @books[1])
+  end
 
+  def test_return_all_info__book_not_found
+    library = Library.new(@books)
+    found_book = library.return_all_info("new_moon")
+    assert_nil(found_book)
+  end
 
+  def test_return_rental_details__found
+    library = Library.new(@books)
+    details = library.return_rental_details("harry_potter")
+    assert_equal(details, @books[1][:rental_details])
+  end
+
+  def test_return_rental_details__not_found
+    library = Library.new(@books)
+    details = library.return_rental_details("new_moon")
+    assert_nil(details)
+  end
+
+  def test_checkout_new_book
+    library = Library.new(@books)
+    library.checkout_book("the_twits")
+    assert_equal("the_twits", library.books[3][:title])
+  end
+
+  def test_add_rental_details
+    library = Library.new(@books)
+    library.checkout_book("the_twits")
+    library.add_rental_details("the_twits", "Jane", "02/08/17")
+    assert_equal("Jane", library.books[3][:rental_details][:student_name])
+    assert_equal("02/08/17", library.books[3][:rental_details][:date])
+  end
 end
