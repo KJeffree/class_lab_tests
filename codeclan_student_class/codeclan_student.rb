0341157827db0731aab4ca0c5d01ffc1dafbe7b8
Katie Jeffree <k.jeffree@gmail.com>
2018-09-17 14:50:03 +0100

    first commit

---
+++ codeclan_student_class/codeclan_student.rb
@@ -0,0 +1,12 @@
+class Student
+  attr_accessor :name, :cohort
+  def initialize(name, cohort)
+    @name = name
+    @cohort = cohort
+  end
+
+  def talk()
+    return "I can talk"
+  end
+
+end
---
+++ codeclan_student_class/specs/codeclan_student_test.rb
@@ -0,0 +1,33 @@
+require('minitest/autorun')
+require('minitest/rg')
+require_relative('../codeclan_student')
+
+class TestBankAccount < MiniTest::Test
+  def test_name_of_student
+    student = Student.new("John", "E25")
+    assert_equal("John", student.name)
+  end
+
+  def test_cohort_name
+    student = Student.new("John", "E25")
+    assert_equal("E25", student.cohort)
+  end
+
+  def test_change_name
+    student = Student.new("John", "E25")
+    student.name = "Paul"
+    assert_equal("Paul", student.name)
+  end
+
+  def test_change_cohort
+    student = Student.new("John", "E25")
+    student.cohort = "E26"
+    assert_equal("E26", student.cohort)
+  end
+
+  def test_talk
+    student = Student.new("John", "E25")
+    assert_equal("I can talk", student.talk)
+  end
+
+end
